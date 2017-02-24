3abd73426c855b4a7721d3664f1f83b89b631851
Bartosz <bartek.g55@gmail.com>
2017-02-24 19:21:56 +0000

    Added template to MainController

--- src/callib/Controllers/MainController.java
+++ src/callib/Controllers/MainController.java
@@ -5,12 +5,14 @@
  */
 package callib.Controllers;
 
-import callib.Models.DBConnector;
+//import callib.Models.DBConnector;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,20 +23,14 @@ public class MainController extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent event) -> {
-            System.out.println("Hello World!");
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            Parent home_page_parent = FXMLLoader.load(getClass().getResource("/callib/Views/Main.fxml"));
+            Scene home_page_scene = new Scene(home_page_parent);
+            primaryStage.setScene(home_page_scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
