6730b0ac5a8c1b5cb6c6fd642e5158685b44a365
Bartosz <bartek.g55@gmail.com>
2017-03-04 20:50:50 +0000

    Book search model logic done.

--- src/callib/Controllers/SearchController.java
+++ src/callib/Controllers/SearchController.java
@@ -5,9 +5,13 @@
  */
 package callib.Controllers;
 
+import callib.Models.Book;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +19,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import java.util.ArrayList;
+import javafx.scene.control.TableColumn;
+//import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -24,10 +32,30 @@ import javafx.stage.Stage;
  */
 public class SearchController implements Initializable {
 
-    @FXML
-    private Label label;
+    private Book book = Book.getInstance();
     private Stage stage;
     private Parent root;
+    
+    @FXML
+    private TableView table;
+    @FXML
+    private TableColumn title;
+    @FXML
+    private TableColumn category;
+    @FXML
+    private TableColumn author;
+    @FXML
+    private TableColumn isbn;
+    @FXML
+    private TableColumn publisher;
+    @FXML
+    private TableColumn date;
+    @FXML
+    private TableColumn pages;
+    @FXML
+    private TableColumn quantity;
+    @FXML
+    private Label label;
     @FXML
     private void back(ActionEvent event) throws IOException {
         stage = (Stage) label.getScene().getWindow();
@@ -41,7 +69,10 @@ public class SearchController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        ObservableList<ArrayList> data = FXCollections.observableArrayList(book.getBookList());
+//        title.setCellValueFactory(new PropertyValueFactory<ArrayList,String>());
+//        table.setItems(data);
+//        table.getColumns().addAll(title);
     }    
     
 }
--- src/callib/Models/Book.java
+++ src/callib/Models/Book.java
@@ -5,10 +5,49 @@
  */
 package callib.Models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Admin
  */
 public class Book {
     
+    private DBConnector connector = DBConnector.getInstance();
+    
+    private static Book book = new Book();
+    
+    private Book() {}
+    
+    public static Book getInstance() {
+        return book;
+    }
+    
+    public ArrayList<ArrayList<String>> getBookList() {
+        ResultSet rs = connector.executeSelectStatement("SELECT * FROM books");
+        int numcols;
+        try {
+            numcols = rs.getMetaData().getColumnCount();
+            ArrayList<ArrayList<String>> result = new ArrayList<>();
+
+            while (rs.next()) {
+                ArrayList<String> row = new ArrayList<>(numcols); // new list per row
+
+                for (int i=1; i<= numcols; i++) {  // don't skip the last column, use <=
+                    row.add(rs.getString(i));
+                    System.out.print(rs.getString(i) + "\t");
+                }
+                result.add(row); // add it to the result
+                System.out.print("\n");
+            }
+            return result;
+        } catch (SQLException ex) {
+            Logger.getLogger(Book.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;     
+    }
 }
