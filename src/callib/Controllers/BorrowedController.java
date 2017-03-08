4c5d2d89205e7a62e41a423ba4fbe7109dabd03c
Bartosz <bartek.g55@gmail.com>
2017-03-08 20:46:28 +0000

    Done action when row in table is double clicked. Setting cell color according to date still to do.

--- src/callib/Controllers/BorrowedController.java
+++ src/callib/Controllers/BorrowedController.java
@@ -10,6 +10,7 @@ import callib.Models.BorrowedBook;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Date;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,9 +20,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /**
@@ -36,21 +39,29 @@ public class BorrowedController implements Initializable {
     private Parent root;
     
     @FXML
-    private TableView table;
+    private TableView<BorrowedBookEntity> table;
     @FXML
-    private TableColumn title;
+    private TableColumn<BorrowedBookEntity, String> title;
     @FXML
-    private TableColumn category;
+    private TableColumn<BorrowedBookEntity, String> category;
     @FXML
-    private TableColumn author;
+    private TableColumn<BorrowedBookEntity, String> author;
     @FXML
-    private TableColumn isbn;
+    private TableColumn<BorrowedBookEntity, Integer> isbn;
     @FXML
-    private TableColumn publisher;
+    private TableColumn<BorrowedBookEntity, String> publisher;
     @FXML
-    private TableColumn date;
+    private TableColumn<BorrowedBookEntity, Date> date;
     @FXML
-    private TableColumn return_date;
+    private TableColumn<BorrowedBookEntity, Date> return_date;
+    
+    @FXML
+    public void clickItem(MouseEvent event)
+    {
+        if (event.getClickCount() == 2)
+            System.out.println(table.getSelectionModel().getSelectedItem().getId());
+            // TODO Open new window with informations and options (renew period of use, do some other stuff)
+    }
     
     @FXML
     private Label label;
@@ -75,7 +86,18 @@ public class BorrowedController implements Initializable {
         isbn.setCellValueFactory(new PropertyValueFactory<>("isbn"));
         publisher.setCellValueFactory(new PropertyValueFactory<>("publisher"));
         date.setCellValueFactory(new PropertyValueFactory<>("date"));
-        return_date.setCellValueFactory(new PropertyValueFactory<>("return_date"));
+        //return_date.setCellValueFactory(new PropertyValueFactory<>("return_date"));
+        
+        
+        return_date.setCellFactory((TableColumn<BorrowedBookEntity, Date> column) -> {
+            return new TableCell<BorrowedBookEntity, Date>() {
+                @Override
+                protected void updateItem(Date item, boolean empty) {
+                    super.updateItem(item, empty);
+                    setText(empty ? "" : "some date");
+                }
+            };
+        });
  
         table.setItems(data);
     }    
